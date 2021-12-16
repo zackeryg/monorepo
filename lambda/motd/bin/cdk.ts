1f4724ba03fa8b5af71d6822878b411e5b0f09cd
Zackery Griesinger <zack.griesinger@c2fo.com>
2021-12-16 08:12:04 -0600

    Add more to motd

--- lambda/motd/bin/cdk.ts
+++ lambda/motd/bin/cdk.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { LambdaStack, cdk } from '@zgriesinger/cdk';
+import * as path from 'path';
 
 const app = new cdk.App();
 
 new LambdaStack(app, 'motd', {
-  pathToCode: path.join('../dist')
+  pathToCode: path.join('../dist'),
   env: {
     account: '436288151216',
     region: 'us-east-1' 
