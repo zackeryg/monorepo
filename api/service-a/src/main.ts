422b295665e553edd44c29122ede0343e654229a
Zack Griesinger <zack.griesinger@c2fo.com>
2022-03-04 09:52:12 -0600

    Add transitive dep example

--- api/service-a/src/main.ts
+++ api/service-a/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from '@zgriesinger/logger';
 
 async function bootstrap() {
   // Add a comment
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true
+    origin: true,
   });
   await app.listen(3000);
 }
