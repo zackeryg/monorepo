5d11f6b9fe4bea796b09578db6aaac98314f71dc
Zackery Griesinger <zgriesinger@gmail.com>
2024-07-22 15:42:34 -0500

    Convert to pnpm

--- README.md
+++
@@ -1,7 +0,0 @@
-# monorepo
-Npm Workspaces Mono Repo
-From the root run:
-```
-npm i -g npm@7 # or 8
-npm i
-```
--- packages/backend/logger/src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomLogger } from './index';
-
-it('Test that the logger prints out the custom startup message', () => {
-  const logger = new CustomLogger();
-  let log = jest.spyOn(logger, 'log');
-  logger.customLog();
-  expect(log).toHaveBeenCalledWith('This is the custom log!');
-});
